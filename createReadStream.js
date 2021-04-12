74cda16e81ba252c7a898b774e393644c25379f5
211211 <nhquan307@outlook.com>
2021-04-13 02:46:24 +0700

    Use less memory to handle large file

---
+++ createReadStream.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  const src = fs.createReadStream('./big.file');
+  src.pipe(res);
+});
+
+console.log('createReadStream.js is running!')
+server.listen(8000);
